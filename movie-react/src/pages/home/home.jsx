ce66ff37a7081fa3b0d1927c76b58c6f49bca459
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-03-17 17:57:10 -0700

    implemented featured movie

--- movie-react/src/pages/home/home.jsx
+++ movie-react/src/pages/home/home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 // import Navbar from "../../components/navbar/Navbar";
-// import Featured from "../../components/featured/Featured";
+import Featured from "../../components/featured/Featured";
 import "./home.scss";
 // import List from "../../components/list/List";
 
 const Home = () => {
   return (
     <div className="home">
-      {/* <Navbar />
       <Featured />
+      {/* <Navbar />
       <List />
       <List />
       <List />
