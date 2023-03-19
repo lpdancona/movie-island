2605dfbfbcc12166bdd18305f334b09908280cea
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-03-19 03:25:58 -0700

    Create register page and add scss styling

--- movie-react/src/App.jsx
+++ movie-react/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Home from "./pages/home/home";
+import Register from "./pages/register/Register";
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    // <div className="App">
+    //   <Home />
+    // </div>
+    <Register/>
   );
 }
 
