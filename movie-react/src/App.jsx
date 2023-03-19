295e12ae320199df6ead45f0cf7dece360024331
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-03-19 03:48:57 -0700

    Add Login page and scss styling

--- movie-react/src/App.jsx
+++ movie-react/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.scss";
 import Home from "./pages/home/home";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+
 function App() {
   return (
     // <div className="App">
     //   <Home />
     // </div>
-    <Register/>
+    // <Register/>
+    <Login/>
   );
 }
 
